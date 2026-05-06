308b6cb343fc3af51a62a309c37b7e439f841194
Lixian Su <lixiansu.ac@gmail.com>
2026-05-06 17:29:18 +0800

    1

--- VLLM/__init__.py
+++ VLLM/__init__.py
@@ -1,7 +1,6 @@
 # 文件位置: vllm/__init__.py
 
 # 直接从同级目录下的脚本导入清洗函数
-from qwen25vl import qwen25vl
 from qwen25vl import setup_controlled_vlm
 from qwen25vl import qwen25vl_to_shapefile
 # 随着你的进度，未来你可以直接在这里继续追加：
@@ -9,7 +8,6 @@ from qwen25vl import qwen25vl_to_shapefile
 # from .spatial_ops import extract_and_aggregate_features
 
 __all__ = [
-    "qwen25vl",
     "setup_controlled_vlm",
     "qwen25vl_to_shapefile"
 ]
